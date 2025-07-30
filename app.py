b7cb08cbc9f86481ea264213aeeac6ec72e574c0
rajthilak2124 <rajthilak.j@gmail.com>
2025-07-31 00:29:16 +0530

    commit101

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Welcome to Karunya Monolithic App!"
+    return "Welcome to Karunya Monolithic Application!"
 
 @app.route("/products")
 def products():
